b4b893a22e52e5269e5993cd6ab3aa212e1bccf6
Sunil Kumar <sunil11qq11@gmail.com>
2026-05-23 13:12:45 +0530

    fixed inngest server crash

--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
 import express from 'express';
 import dotenv from "dotenv";
 dotenv.config();
+
 import cors from 'cors';
-import { clerkMiddleware } from '@clerk/express'
-import {sever} from "inngest/express";
-import {inngest, functions} from "./inngest/index.js"
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
-app.use(clerkMiddleware())
+app.use(cors());
+app.use(clerkMiddleware());
 
-app.get('/', (req, res)=> res.send('Server is live!'))
+app.get('/', (req, res) => res.send('Server is live!'));
 
-app.get("/api/inngest", serve({client: inngest, functions}));
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+export default app;
