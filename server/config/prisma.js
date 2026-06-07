4f6d1b3d0df81965352dd45a33c4e74fe69072c6
Sunil Kumar <sunil11qq11@gmail.com>
2026-06-07 10:23:01 +0530

    fix prisma neon adapter

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -1,23 +1,18 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
-import { neonConfig } from '@neondatabase/serverless';
-
+import { Pool, neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
 
-const connectionString = process.env.DATABASE_URL;
-
-const adapter = new PrismaNeon({
-  connectionString,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+const adapter = new PrismaNeon(pool);
+
+const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV === 'development') {
   global.prisma = prisma;
